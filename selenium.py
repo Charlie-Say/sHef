7c919f91407f67674e29b8374f65f3f32b605025
Charlie-Say <charliesay1990@hotmail.com>
2021-03-08 16:49:06 -0800

    testing requests

---
+++ selenium.py
@@ -0,0 +1,16 @@
+"""
+This program will go through site and input the given key word from voice recognition
+and submit it to find the url. The url will be passed to the scrape.py module where it will
+scrape the page of the given key word/s.
+
+---------------- Psuedocode -----------
+
+This is for a single item/word:
+    - import selenium
+    - identify search bar
+    - input key word
+    - click submit
+
+This is for detail of item (foodprint.org):
+    - 
+"""
